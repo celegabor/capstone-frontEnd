895defee62692cc0ff4482573b611cee71397f0b
Celestin Gabor <127986056+celegabor@users.noreply.github.com>
2023-11-04 21:36:08 +0100

    aggiunta sezione messaggi 2.0 piu varie modifiche

--- src/components/R-sidebar/MyR-Sidebar.js
+++ src/components/R-sidebar/MyR-Sidebar.js
@@ -3,46 +3,47 @@ import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { PostProvider } from '../../context/context';
 import useSession from '../../hooks/useSession';
-import { faMessage } from '@fortawesome/free-solid-svg-icons';
+import { faMessage, faEdit, faCogs } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
-import MessagePopup from '../messagePopup/messagePopup'
-
-
-import './sidebarR.css'
-
+import MessagePopup from '../messagePopup/messagePopup';
+import './sidebarR.css';
+import { useNavigate, Link } from 'react-router-dom';
 
 const OffCanvasRight = ({ name, ...props }) => {
 
-    const { showR, setShowR, handleCloseR, handleShowR } = useContext(PostProvider);
-    const session = useSession()  
-    const [users, setUsers] = useState([]);
-    const [showMessagePopup, setShowMessagePopup] = useState(false);
-    const [messageRecipient, setMessageRecipient] = useState(null);
+  const { showR, setShowR, handleCloseR, handleShowR } = useContext(PostProvider);
+  const session = useSession();
+  const [users, setUsers] = useState([]);
+  const [messageRecipient, setMessageRecipient] = useState(null);
 
-    const handleShowMessagePopup = (userId, userName, userLastName, userAvatar, fromName, fromLastName, fromAvatar) => {
-      setMessageRecipient({ 
-        from: session.id, 
-        to: userId, 
-        name: userName,
-        lastName: userLastName,
-        avatar: userAvatar,
-        fromName: fromName,
-        fromLastName: fromLastName,
-        fromAvatar: fromAvatar,
+  const handleShowMessagePopup = (userId, userName, userLastName, userAvatar, fromName, fromLastName, fromAvatar) => {
+    setMessageRecipient({
+      from: session.id,
+      to: userId,
+      name: userName,
+      lastName: userLastName,
+      avatar: userAvatar,
+      fromName: fromName,
+      fromLastName: fromLastName,
+      fromAvatar: fromAvatar,
+    });
+
+    console.log(fromName,fromLastName,fromAvatar);
+  };
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/users2/get`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.users);
+      })
+      .catch((error) => {
+        console.error('Errore nel recupero degli utenti:', error);
       });
-    };    
+  }, []);
 
-    useEffect(() => {
-      fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/users2/get`)
-        .then((response) => response.json())
-        .then((data) => {
-          setUsers(data.users);
-        })
-        .catch((error) => {
-          console.error('Errore nel recupero degli utenti:', error);
-        });
-    }, []);
   return (
     <>
       <div className='bg-custom'>
@@ -52,49 +53,59 @@ const OffCanvasRight = ({ name, ...props }) => {
           </Offcanvas.Header>
           <Offcanvas.Header key="user-list-header" className='bg-secondary'>
             <Offcanvas.Title>
-            {users.map((user, index) => (
-              <div key={`user-${index}`}>
-                {user._id !== session.id ? (
-                  null
-                ) : (
-                  <div key={user._id} className="user-card w-100 p-3 d-flex align-items-center border-bottom border-1 border-dark ">
-                    <img className='img-custom-users object-fit-contain' src={user.avatar} alt={`Avatar di ${user.name}`} />
-                    <p className='ms-4'>{user.name} {user.lastName}</p>
-                  </div>
-                )}
-              </div>
-            ))}
+              {users.map((user) => (
+                <div key={user._id}>
+                  {user._id === session.id ? (
+                    <div className='d-flex align-items-center'>
+                      <div key={user._id} className="user-card w-100 p-3 d-flex align-items-center border-bottom border-1 border-dark">
+                        <Link className='w-100 d-flex align-items-center text-dark text-decoration-none' to={`/user`}>
+                          <img className='img-custom-users object-fit-contain' src={user.avatar} alt={`Avatar di ${user.name}`} />
+                          <p className='ms-2'>{user.name} {user.lastName}</p>
+                        </Link>
+                      </div>
+                      <div className='d-flex'>
+                         <Button variant='secondary' className='border border-dark border-2 mx-2' onClick={() => {
+                           navigate('/user');
+                         }}>
+                           <FontAwesomeIcon icon={faEdit} />
+                         </Button>
+                         {/* <Button variant='secondary' className='border border-dark border-2 mx-2'>
+                           <FontAwesomeIcon icon={faCogs} />
+                         </Button> */}
+                       </div>
+                    </div>
+                  ) : null}
+                </div>
+              ))}
             </Offcanvas.Title>
           </Offcanvas.Header>
 
           <Offcanvas.Body className='bg-custom text-light'>
-            
             <div className="user-list">
-            {users.map((user) => (
-              <div key={user._id}>
-                {user._id === session.id ? (
-                  null
-                ) : (
-                  <div className='d-flex align-items-center'>
-                    <Link className='text-decoration-none text-light w-75' to={`/getUserId/${user._id}`}>
-                      <div className="user-card w-100 p-3 d-flex align-items-center border-bottom border-1 border-dark ">
-                        <img className="img-custom-users object-fit-contain" src={user.avatar} alt={`Avatar di ${user.name}`} />                    
-                        <p className='ms-4'>{user.name} {user.lastName}</p>
+              {users.map((user) => (
+                <div key={user._id}>
+                  {user._id === session.id ? (
+                    null
+                  ) : (
+                    <div className='d-flex align-items-center'>
+                      <Link className='w-100 text-light text-decoration-none ' to={`/getUserId/${user._id}`}>
+                        <div key={user._id} className="user-card w-100 p-3 d-flex align-items-center border-bottom border-1 border-dark">
+                          <img className='img-custom-users object-fit-contain border border-1' src={user.avatar} alt={`Avatar di ${user.name}`} />
+                          <p className='ms-4'>{user.name} {user.lastName}</p>
+                        </div>
+                      </Link>
+                      <div className='d-flex'>
+                        <Button variant='secondary' className='border border-dark border-2 mx-2' onClick={() => handleShowMessagePopup(user._id, user.name, user.lastName, user.avatar, session.name, session.lastName, session.avatar)}>
+                          <FontAwesomeIcon icon={faMessage} />
+                        </Button>
                       </div>
-                    </Link>
-                    <Button
-                      variant="secondary"
-                      onClick={() => handleShowMessagePopup(user._id, user.name, user.lastName, user.avatar, session.name, session.lastName, session.avatar )}
-                    >
-                      <FontAwesomeIcon icon={faMessage} />
-                    </Button>
-                  </div>
+                    </div>
                   )}
                 </div>
               ))}
-
             </div>
           </Offcanvas.Body>
+
         </Offcanvas>
       </div>
       {messageRecipient && (
@@ -109,7 +120,6 @@ const OffCanvasRight = ({ name, ...props }) => {
           fromName={messageRecipient.fromName}
           fromLastName={messageRecipient.fromLastName}
           fromAvatar={messageRecipient.fromAvatar}
-
         />
       )}
     </>
@@ -117,13 +127,12 @@ const OffCanvasRight = ({ name, ...props }) => {
 }
 
 function Right() {
-
   return (
-      <>
-          {['end'].map((placement, idx) => (
-              <OffCanvasRight key={idx} placement={placement} name={placement} />
-          ))}
-      </>
+    <>
+      {['end'].map((placement, idx) => (
+        <OffCanvasRight key={idx} placement={placement} name={placement} />
+      ))}
+    </>
   );
 }
 
